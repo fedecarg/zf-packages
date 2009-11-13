8df08f8f84d3da201f617213a154c7aa54eb05b2
fede <fede@80e17c93-eb1c-44a2-a00e-b391dc33b984>
2009-11-13 15:53:47 +0000

    Added standard dispatcher for unit testing
    git-svn-id: http://svn.fedecarg.com/repo/Zf@131 80e17c93-eb1c-44a2-a00e-b391dc33b984

---
+++ Controller/Dispatcher/StandardTestCase.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * Zf library
+ *
+ * @category    Zf
+ * @package     Zf_Controller
+ * @subpackage  Dispatcher
+ * @author      Federico Cargnelutti <fedecarg@gmail.com>
+ * @version     $Id: $
+ */
+
+/**
+ * @category    Zf
+ * @package     Zf_Controller
+ * @subpackage  Dispatcher
+ * @author      Federico Cargnelutti <fedecarg@gmail.com>
+ * @version     $Id: $
+ */
+class Zf_Controller_Dispatcher_StandardTestCase extends Zend_Controller_Dispatcher_Standard
+{
+    /**
+     * Dispatch to a controller/action
+     *
+     * By default, if a controller is not dispatchable, dispatch() will throw
+     * an exception. If you wish to use the default controller instead, set the
+     * param 'useDefaultControllerAlways' via {@link setParam()}.
+     *
+     * @param Zend_Controller_Request_Abstract $request
+     * @param Zend_Controller_Response_Abstract $response
+     * @return void
+     * @throws Zend_Controller_Dispatcher_Exception
+     */
+    public function dispatch(Zend_Controller_Request_Abstract $request, Zend_Controller_Response_Abstract $response)
+    {
+        $this->setResponse($response);
+
+        /**
+         * Get controller class
+         */
+        if (!$this->isDispatchable($request)) {
+            $controller = $request->getControllerName();
+            if (!$this->getParam('useDefaultControllerAlways') && !empty($controller)) {
+                require_once 'Zend/Controller/Dispatcher/Exception.php';
+                throw new Zend_Controller_Dispatcher_Exception('Invalid controller specified (' . $request->getControllerName() . ')');
+            }
+
+            $className = $this->getDefaultControllerClass($request);
+        } else {
+            $className = $this->getControllerClass($request);
+            if (!$className) {
+                $className = $this->getDefaultControllerClass($request);
+            }
+        }
+
+        /**
+         * Load the controller class file
+         */
+        $className = $this->loadClass($className);
+
+        /**
+         * Instantiate controller with request, response, and invocation
+         * arguments; throw exception if it's not an action controller
+         */
+        $controller = new $className($request, $this->getResponse(), $this->getParams());
+        if (!$controller instanceof Zend_Controller_Action) {
+            require_once 'Zend/Controller/Dispatcher/Exception.php';
+            throw new Zend_Controller_Dispatcher_Exception("Controller '$className' is not an instance of Zend_Controller_Action");
+        }
+
+        /**
+         * Retrieve the action name
+         */
+        $action = $this->getActionMethod($request);
+
+        /**
+         * Dispatch the method call
+         */
+        $request->setDispatched(true);
+
+        // by default, buffer output
+        $disableOb = $this->getParam('disableOutputBuffering');
+        $obLevel   = ob_get_level();
+        if (empty($disableOb)) {
+            ob_start();
+        }
+
+        try {
+            $controller->dispatch($action);
+        } catch (Exception $e) {
+            // Clean output buffer on error
+            $curObLevel = ob_get_level();
+            if ($curObLevel > $obLevel) {
+                do {
+                    ob_get_clean();
+                    $curObLevel = ob_get_level();
+                } while ($curObLevel > $obLevel);
+            }
+
+            throw $e;
+        }
+
+        if (empty($disableOb)) {
+            $content = ob_get_clean();
+            $response->appendBody($content);
+        }
+
+        Zend_Registry::set('Zend_Controller_Action', $controller);
+        
+        // Destroy the page controller instance and reflection objects
+        $controller = null;
+    }
+}
